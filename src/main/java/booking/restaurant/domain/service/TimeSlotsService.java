980fdbb2fd589b230be8b7cd11e106e50c8f0d96
samer955 <samer.osman95@hotmail.com>
2022-04-26 18:33:25 +0200

    TimeSlotService angepasst mit start und ende Reservierung vom prop.

--- src/main/java/booking/restaurant/domain/service/TimeSlotsService.java
+++ src/main/java/booking/restaurant/domain/service/TimeSlotsService.java
@@ -1,10 +1,8 @@
 package booking.restaurant.domain.service;
 
+import booking.restaurant.domain.ReservationTime;
 import booking.restaurant.domain.model.Reservation;
 import booking.restaurant.domain.model.TimeSlot;
-import org.springframework.stereotype.Service;
-
-
 import java.time.LocalDate;
 import java.time.LocalTime;
 import java.time.temporal.ChronoUnit;
@@ -14,9 +12,11 @@ import java.util.stream.Collectors;
 
 public class TimeSlotsService {
 
-    //TODO: variable should be in the property and then injected
-    LocalTime startReservation = LocalTime.of(9, 0);
-    LocalTime endReservation = LocalTime.of(22, 0);
+    private final ReservationTime reservationTime;
+
+    public TimeSlotsService(ReservationTime reservationTime) {
+        this.reservationTime = reservationTime;
+    }
 
     public List<TimeSlot> getFreeTimeSlots(List<Reservation> reservationsOnDay, LocalDate date, int tableNumber) {
 
@@ -29,7 +29,7 @@ public class TimeSlotsService {
             return getTimeSlotsFromEmptyList(date, tableNumber, freeTimeSlots);
         }
 
-        if (startReservation.minus(2,ChronoUnit.HOURS).isAfter(startReservation)) {
+        if (reservationHours.get(0).minus(2,ChronoUnit.HOURS).isAfter(reservationTime.start())) {
             getTimeSlotsFromStart(date, tableNumber, freeTimeSlots, reservationHours);
         }
 
@@ -39,7 +39,7 @@ public class TimeSlotsService {
 
         LocalTime last_tmp = reservationHours.get(reservationHours.size() - 1).plus(2, ChronoUnit.HOURS);
 
-        if (last_tmp.isBefore(endReservation)) {
+        if (last_tmp.isBefore(reservationTime.end())) {
             getTimeSlotsTillEnd(date, tableNumber, freeTimeSlots, last_tmp);
         }
 
@@ -68,7 +68,7 @@ public class TimeSlotsService {
     //create TimeSlots from last reservation till the end of the day
     private void getTimeSlotsTillEnd(LocalDate date, int tableNumber, List<TimeSlot> freeTimeSlots, LocalTime last_tmp) {
 
-        while (last_tmp.isBefore(endReservation) || last_tmp.equals(endReservation)) {
+        while (last_tmp.isBefore(reservationTime.end()) || last_tmp.equals(reservationTime.end())) {
             TimeSlot timeslot = new TimeSlot(date, last_tmp, tableNumber);
             freeTimeSlots.add(timeslot);
             last_tmp = last_tmp.plus(15, ChronoUnit.MINUTES);
@@ -79,7 +79,7 @@ public class TimeSlotsService {
     private void getTimeSlotsFromStart(LocalDate date, int tableNumber, List<TimeSlot> freeTimeSlots, List<LocalTime> reservationHours) {
         LocalTime start_tmp = reservationHours.get(0).minus(2, ChronoUnit.HOURS);
 
-        while (start_tmp.isAfter(startReservation) || start_tmp.equals(startReservation)) {
+        while (start_tmp.isAfter(reservationTime.start()) || start_tmp.equals(reservationTime.start())) {
             TimeSlot timeslot = new TimeSlot(date, start_tmp, tableNumber);
             freeTimeSlots.add(timeslot);
             start_tmp = start_tmp.minus(15, ChronoUnit.MINUTES);
@@ -89,9 +89,9 @@ public class TimeSlotsService {
     //create all possible timeslots (15 min blocks)
     private List<TimeSlot> getTimeSlotsFromEmptyList(LocalDate date, int tableNumber, List<TimeSlot> freeTimeSlots) {
         
-        LocalTime start_tmp = startReservation;
+        LocalTime start_tmp = reservationTime.start();
 
-        while (start_tmp.isBefore(endReservation) || start_tmp.equals(endReservation)) {
+        while (start_tmp.isBefore(reservationTime.end()) || start_tmp.equals(reservationTime.end())) {
             TimeSlot timeSlot = new TimeSlot(date, start_tmp, tableNumber);
             freeTimeSlots.add(timeSlot);
             start_tmp = start_tmp.plus(15, ChronoUnit.MINUTES);
