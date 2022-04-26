72bac56fe4c7f23da0aa9e30e82818d9f8ab4b51
samer955 <samer.osman95@hotmail.com>
2022-04-26 21:45:51 +0200

    Konfigurationsdatei für Tests.

---
+++ src/test/resources/loadtest.sql
@@ -0,0 +1,40 @@
+CREATE TABLE IF NOT EXISTS `table_dto` (
+    id int primary key auto_increment,
+    number int unique,
+    min_capacity int,
+    max_capacity int
+);
+
+
+CREATE TABLE IF NOT EXISTS `reservation_dto`(
+    id int primary key auto_increment,
+    code text,
+    date date,
+    time time,
+    note text null,
+    persons int,
+    table_number int
+);
+
+CREATE TABLE IF NOT EXISTS `customer`(
+    reservation_dto int primary key references reservation_dto(id),
+    name varchar(100),
+    email varchar(100),
+    telefon_number varchar(20)
+    );
+
+
+INSERT INTO `table_dto` (id, number, min_capacity, max_capacity)
+VALUES (1, 1, 5, 6);
+
+INSERT INTO `table_dto` (id, number, min_capacity, max_capacity)
+VALUES (2, 2, 4, 5);
+
+INSERT INTO `table_dto` (id, number, min_capacity, max_capacity)
+VALUES (3, 3, 3, 4);
+
+INSERT INTO `table_dto` (id, number, min_capacity, max_capacity)
+VALUES (4, 4, 1, 2);
+
+INSERT INTO `table_dto` (id, number, min_capacity, max_capacity)
+VALUES (5, 5, 1, 2);
