217333ec11e7b41fc560b2a2af9143ff8890bb98
samer955 <samer.osman95@hotmail.com>
2022-04-27 22:12:30 +0200

    new message exception added

--- src/main/java/booking/restaurant/domain/exception/ReservationException.java
+++ src/main/java/booking/restaurant/domain/exception/ReservationException.java
@@ -8,6 +8,12 @@ public class ReservationException extends Throwable {
     public static final String ERROR_MSG_CANCEL_BOOKING =
             "You cannot cancel your booking, as there are less than 2 hours to your reservation";
 
+    public static final String ERROR_MSG_WRONG_CODE =
+            "The code you entered is incorrect";
+
+    public static final String ERR_NO_TIME_SLOT_AVAILABLE =
+            "There are no free time Slots for this Hour";
+
     public ReservationException(String errorMessage) {
         super(errorMessage);
     }
