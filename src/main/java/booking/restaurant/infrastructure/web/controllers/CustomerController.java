840fc83d128b3390193fb4c1617d4d1ac104389e
samer955 <samer.osman95@hotmail.com>
2022-04-27 22:16:05 +0200

    CustomerController close booking implementiert

--- src/main/java/booking/restaurant/infrastructure/web/controllers/CustomerController.java
+++ src/main/java/booking/restaurant/infrastructure/web/controllers/CustomerController.java
@@ -2,15 +2,19 @@ package booking.restaurant.infrastructure.web.controllers;
 
 import booking.restaurant.domain.exception.ReservationException;
 import booking.restaurant.domain.model.TimeSlot;
+import booking.restaurant.infrastructure.web.CustomerInfoForm;
 import booking.restaurant.infrastructure.web.FormularForm;
 import booking.restaurant.service.BookingService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.servlet.http.HttpSession;
+import java.time.LocalDate;
+import java.time.LocalTime;
 import java.util.List;
 
 import static booking.restaurant.domain.exception.ReservationException.ERR_NO_TIME_SLOT_AVAILABLE;
@@ -51,5 +55,24 @@ public class CustomerController {
         }
         model.addAttribute("timeslots", freeSlots);
         return "timeslots";
+    }
+
+    @GetMapping("/book/{table}")
+    public String getReservationView(@PathVariable("table") int tableNumber, Model model) {
+        model.addAttribute("table", tableNumber);
+        return "reservation";
+    }
+
+    @PostMapping("/close/booking/{table}")
+    public String postReservation(@PathVariable("table") int tableNumber, Model model, CustomerInfoForm form, HttpSession session) {
+
+        LocalDate date = (LocalDate) session.getAttribute("date");
+        LocalTime time = (LocalTime) session.getAttribute("time");
+        int persons = (int) session.getAttribute("persons");
+
+        TimeSlot timeSlot = new TimeSlot(date,time,tableNumber);
+        bookingService.closeBooking(form.toCustomer(),null,persons,timeSlot);
+        model.addAttribute("success", " Thank you for your booking! A confirmation Email was sended to your Email account.");
+        return "message";
     }
 }
