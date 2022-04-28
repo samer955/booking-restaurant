3f8d7cdbb26187e57c4a2e1991827c6b8d0d885f
samer955 <samer.osman95@hotmail.com>
2022-04-28 10:23:10 +0200

    persistence constructor bei reservationDto added

--- src/main/java/booking/restaurant/infrastructure/persistence/reservation/ReservationDTO.java
+++ src/main/java/booking/restaurant/infrastructure/persistence/reservation/ReservationDTO.java
@@ -3,9 +3,11 @@ package booking.restaurant.infrastructure.persistence.reservation;
 import booking.restaurant.domain.model.Customer;
 import booking.restaurant.domain.model.Reservation;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.annotation.PersistenceConstructor;
 
 import java.time.LocalDate;
 import java.time.LocalTime;
+import java.util.Objects;
 
 public class ReservationDTO {
 
@@ -18,6 +20,18 @@ public class ReservationDTO {
     private String note;
     private int persons;
     private int tableNumber;
+
+    @PersistenceConstructor
+    public ReservationDTO(Long id, String code, Customer customer, LocalDate date, LocalTime time, String note, int persons, int tableNumber) {
+        this.id = id;
+        this.code = code;
+        this.customer = customer;
+        this.date = date;
+        this.time = time;
+        this.note = note;
+        this.persons = persons;
+        this.tableNumber = tableNumber;
+    }
 
     public ReservationDTO(Reservation reservation) {
         this.id = reservation.getId();
@@ -34,4 +48,16 @@ public class ReservationDTO {
         return new Reservation(id, code, customer, date, time, note, persons, tableNumber);
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        ReservationDTO that = (ReservationDTO) o;
+        return persons == that.persons && tableNumber == that.tableNumber && Objects.equals(id, that.id) && Objects.equals(code, that.code) && Objects.equals(customer, that.customer) && Objects.equals(date, that.date) && Objects.equals(time, that.time) && Objects.equals(note, that.note);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, code, customer, date, time, note, persons, tableNumber);
+    }
 }
