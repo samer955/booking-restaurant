62ff81ff17b0eae3886ceaf3d0d876d33fbecf4e
samer955 <samer.osman95@hotmail.com>
2022-04-29 13:35:44 +0200

    h2-console jetzt verfuegbar

--- src/main/java/booking/restaurant/infrastructure/web/securty/WebSecurityConfig.java
+++ src/main/java/booking/restaurant/infrastructure/web/securty/WebSecurityConfig.java
@@ -1,12 +1,17 @@
 package booking.restaurant.infrastructure.web.securty;
 
+import org.apache.catalina.servlets.WebdavServlet;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+
+import javax.servlet.annotation.WebServlet;
 
 
 @EnableWebSecurity
@@ -21,23 +26,29 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
               .roles("ADMIN");
   }
 
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/myrestaurant/**")
+                .permitAll()
+                .antMatchers("/**/admin/**")
+                .authenticated()
+                .and().formLogin();
+        http.csrf().disable();
+    }
 
-  @Override
-  protected void configure(HttpSecurity http) throws Exception {
-      http.authorizeRequests()
-              .antMatchers("/myrestaurant/**")
-              .permitAll()
-              .antMatchers("/**/admin/**")
-              .authenticated()
-              .and().formLogin();
-      http.csrf().disable();
-  }
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring()
+                .antMatchers("/h2-console/**");
+    }
 
 
-  @Bean
-  PasswordEncoder encoder() {
+    @Bean
+    PasswordEncoder encoder() {
       // Das ist nur zum Ausprobieren gedacht!
       // Für Produktion ist das ungeeignet!
       return NoOpPasswordEncoder.getInstance();
   }
+
 }
